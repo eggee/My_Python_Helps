47e7a820b4e1a760b1ee7280af55d4bf4158a654
eggee <timothy.eggenberger@adtran.com>
2017-12-13 09:44:13 -0600

    moving some files around for better organization and reference

--- 01_Subbu_TA5K_example_with_my_edits.py
+++ 01_Subbu_TA5K_example_with_my_edits.py
@@ -1,16 +1,16 @@
-#Exampl test from Subbu (SUBRAMANYAM BATHALA)
+#Example test from Subbu
 from taut_ta5k.communication.ta5k_telnet import Ta5kTelnet
-#from 'this library',                import 'this Class'
 #C:\Python27\Lib\site-packages\taut_ta5k\communication
+from atb.ta5000 import TA5000
 
 setup_dict = {
-    'ip': '10.13.138.101'
+    'ip': '10.13.138.119'
 }
 
 efm_group_param_dict = {
     'shelf': '1',
-    'slot': '14',
-    'group_id': '2',
+    'slot': '13',
+    'group_id': '1',
     'admin_state': 'enable',
     'alias': 'DUMMY',
     'rate_control': 'disable',
@@ -22,11 +22,15 @@ efm_group_param_dict = {
 }
 
 tn = Ta5kTelnet() #define a shorter name
+node1 = TA5000("10.13.138.1")
+
+TA5000.telnet_client()
+
 
 tn.connect(setup_dict['ip'], 'ta5k', timeout=1100) #(self, host, alias, **kwargs)
 
 tn.efm_group_create(efm_group_param_dict)  # configures EFM-Group interface 1/14/2
-
+tn.voice_sip_trunk_create()
 
 efm_dict = tn.efm_group_config_get(efm_group_param_dict)   # getting the EFM-Group interface 1/14/2 parameters
 
--- TimsProject.robot
+++
@@ -1,6 +0,0 @@
-*** Settings ***
-Library           ../../Python27/Lib/Robot_Libraries/abacus/Abacus.py
-
-*** Test Cases ***
-connect
-    connect    localhost    localhost    BBDLC_System_GR303_MGCP_Sip_PulseDial.env    bbdlc
